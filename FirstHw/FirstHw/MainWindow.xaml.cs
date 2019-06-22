8e14c8778e8ee71d9b9df88a4c333f8525bf4b60
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2019-06-22 21:50:21 +0600

    Add files via upload

---
+++ FirstHw/FirstHw/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstHw
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClickOne(object sender, RoutedEventArgs e)
+        {
+            textBlockIp.Text = textBlockIp.Text.Remove(0,textBlockIp.Text.Count());
+            IPHostEntry host = Dns.GetHostEntry(textBoxHost.Text);            
+            foreach (IPAddress ip in host.AddressList)
+                textBlockIp.Text += ip.ToString() + "\n";
+        }
+
+        private void ButtonClickTwo(object sender, RoutedEventArgs e)
+        {            
+            IPHostEntry entry = Dns.GetHostEntry(textBoxIp.Text);
+            textBlockHost.Text = entry.HostName;
+        }
+    }
+}
